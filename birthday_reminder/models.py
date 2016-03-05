b6cec2acce9e5b82105e6fc1c50a2e9c77b61905
Eric Ashmore <edashmore@gmail.com>
2016-03-04 18:48:29 -0800

    feat: generate doctor and patient tables

--- birthday_reminder/models.py
+++ birthday_reminder/models.py
@@ -2,4 +2,13 @@ from __future__ import unicode_literals
 
 from django.db import models
 
-# Create your models here.
+class Doctor(models.Model):
+    first_name = models.CharField(max_length=256)
+    last_name = models.CharField(max_length=256)
+
+class Patient(models.Model):
+    doctor = models.ForeignKey(Doctor)
+    first_name = models.CharField(max_length=256)
+    last_name = models.CharField(max_length=256)
+    email = models.CharField(max_length=256)
+    birthday = models.DateTimeField()
--- drchrono_project/settings.py
+++ drchrono_project/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'birthday_reminder.apps.BirthdayReminderConfig'
 ]
 
 MIDDLEWARE_CLASSES = [
