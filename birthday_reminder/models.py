5357cacb806d6ac407d8d2f37a992ebc7ac9ad18
Eric Ashmore <edashmore@gmail.com>
2016-03-05 17:33:27 -0800

    feat: add email_boolean col to patient

--- birthday_reminder/models.py
+++ birthday_reminder/models.py
@@ -15,6 +15,7 @@ class Patient(models.Model):
     last_name = models.CharField(max_length=256)
     email = models.CharField(max_length=256)
     birthday = models.DateTimeField()
+    email_bool = models.BooleanField('send_email', default=False)
 
     def __str__(self):
         return self.email
--- birthday_reminder/urls.py
+++ birthday_reminder/urls.py
@@ -6,5 +6,5 @@ app_name = 'birthday_reminder'
 urlpatterns = [
     url(r'^$', views.new_session, name='new_session'),
     url(r'^loading', views.loading, name='loading'),
-    url(r'^reminders/(?P<pk>[0-9]+)/$', views.UserView.as_view(), name='user_view'),
+    url(r'^(?P<pk>[0-9]+)/$', views.UserView.as_view(), name='user_view'),
 ]
