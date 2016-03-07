22e1991f1dfa563c70101e919972eb04a20c9ffc
Eric Ashmore <edashmore@gmail.com>
2016-03-07 09:49:04 -0800

    style:add favicon

--- drchrono_project/settings.py
+++ drchrono_project/settings.py
@@ -39,7 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'debug_toolbar',
+    # 'debug_toolbar',
 ]
 
 MIDDLEWARE_CLASSES = [
