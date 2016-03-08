abe03f9873b46dbd94dc09090746473d593b0265
Eric Ashmore <edashmore@gmail.com>
2016-03-07 17:03:23 -0800

    feat(patients): add button to remove all patients from email list

--- birthday_reminder/static/js/birthday_reminder.js
+++ birthday_reminder/static/js/birthday_reminder.js
@@ -1,65 +1,100 @@
-function checkForEmailUpdate() {
-  var $patientList = $('#patient-list');
-  $patientList.on('change', 'input', prepareForSave);
-
+// Add listeners for patient updates
+function listenForPatientUpdate() {
   var $allPatientsButton = $('#email-all-button');
-  $allPatientsButton.on('click', updateAllPatients);
+  $allPatientsButton.on('click', toggleAllPatients);
+
+  var $noPatientsButton = $('#email-none-button');
+  $noPatientsButton.on('click', toggleAllPatients);
+
+  var $patientList = $('#patient-list');
+  $patientList.on('change', 'input', markUserForUpdate);
 }
 
-function updateAllPatients(e) {
+// Update all patients
+function toggleAllPatients(e) {
   e.preventDefault();
+  var sendAll = true;
+  if (e.currentTarget.id === 'email-none-button') {
+    sendAll = false;
+  }
+
   var patients = $('.patient');
-  saveChanges(patients);
+  saveChanges(patients, sendAll);
 }
 
-function prepareForSave(e) {
+function markUserForUpdate(e) {
   var checkbox = e.currentTarget;
-  var patientNode = checkbox.parentNode.parentNode;
-  if (patientNode.classList.contains('changed')) {
-    patientNode.classList.remove('changed');
+  var patientEl = checkbox.parentNode.parentNode;
+  if (patientEl.classList.contains('changed')) {
+    patientEl.classList.remove('changed');
   } else {
-    patientNode.classList.add('changed');
+    patientEl.classList.add('changed');
   }
   activateSaveButton();
 }
 
 function activateSaveButton() {
-  var $button = $('#save-send-changes');
-  if ($button.attr('disabled')) {
-    $button.removeAttr('disabled');
-    $button.addClass('enabled-button');
-    $button.on('click', getChanges);
+  var $saveButton = $('#save-send-changes');
+  if ($saveButton.attr('disabled')) {
+    $saveButton.removeAttr('disabled');
+    $saveButton.addClass('enabled-button');
+    $saveButton.on('click', getMarkedPatients);
   }
 }
 
-function getChanges(e) {
+function getMarkedPatients(e) {
   e.preventDefault();
-  var button = e.currentTarget;
-  button.textContent = 'Saving changes...';
-  button.disabled = true;
+  var saveButton = e.currentTarget;
+  saveButton.disabled = true;
   var $changedPatients = $('#patient-list').find('.changed');
   saveChanges($changedPatients);
 }
 
-function saveChanges($patientsList) {
-  $('#saving-wall').removeClass('display-none');
+function saveChanges($patientsList, boolean) {
+  $('#save-guard').removeClass('display-none');
   var requests = [];
   for (var i = 0; i < $patientsList.length; i++) {
-    requests.push(buildPutRequest($patientsList[i]));
-  }
+    if (boolean === undefined) {
+      boolean = isChecked($patientsList[i]);
+    }
 
+    requests.push(buildPutRequest($patientsList[i], boolean));
+    updatePatientEl($patientsList[i], boolean);
+  }
+  sendRequests(requests);
+}
+
+// Sends all ajax requests at once and runs callback after all are complete
+function sendRequests(requests) {
   $.when.apply($, requests).done(function() {
-    window.location.reload();
+    $('#save-guard').addClass('display-none');
+    successSave();
   });
 }
 
-function buildPutRequest(patient) {
+// Check whether or not a marked patient should be sent an email after update
+function isChecked(patient) {
   var checkbox = patient.querySelector('input');
-  var bool = true;
-  if (patient.classList.contains('changed') && !checkbox.checked) {
-    bool = false;
+  if (checkbox.checked) {
+    return true;
   }
 
+  return false;
+}
+
+// Updates patient DOM element to reflect changes from update
+function updatePatientEl(patient, bool) {
+  var checkbox = patient.querySelector('input');
+  if (bool) {
+    checkbox.checked = true;
+  } else {
+    checkbox.checked = false;
+  }
+
+  patient.classList.remove('changed');
+}
+
+function buildPutRequest(patient, bool) {
   var csrftoken = getCookie('csrftoken');
   return $.ajax({
     url: 'api/patient/' + patient.getAttribute('id') + '/',
@@ -71,7 +106,8 @@ function buildPutRequest(patient) {
   });
 }
 
-function checkForEmailSave() {
+// Add listener for doctor email updates
+function listenForEmailUpdate() {
   var $button = $('#email-submit');
   $button.on('click', saveEmail);
 }
@@ -91,12 +127,15 @@ function saveEmail(e) {
     beforeSend: function(xhr) {
       xhr.setRequestHeader("X-CSRFToken", csrftoken);
     },
-    success: successfulSave(button),
+    success: function() {
+      button.disabled = false;
+      successSave();
+    }
   });
 }
 
-function successfulSave(button) {
-  button.disabled = false;
+// Displays message on successful save
+function successSave() {
   var saveSuccess = $('#save-success');
   saveSuccess.removeClass('display-none');
   setTimeout(function() {
@@ -104,7 +143,7 @@ function successfulSave(button) {
   }, 1500);
 }
 
-//From Django docs https://docs.djangoproject.com/en/1.9/ref/csrf/
+// From Django docs https://docs.djangoproject.com/en/1.9/ref/csrf/
 function getCookie(name) {
     var cookieValue = null;
     if (document.cookie && document.cookie !== '') {
@@ -123,6 +162,6 @@ function getCookie(name) {
 }
 
 (function() {
-  checkForEmailUpdate();
-  checkForEmailSave();
+  listenForPatientUpdate();
+  listenForEmailUpdate();
 })();
