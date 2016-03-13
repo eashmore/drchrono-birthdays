fe7e7a01063a00a2f8d2fb4a146c89cfc9e050ff
Eric Ashmore <edashmore@gmail.com>
2016-03-13 15:18:34 -0700

    refactor(send_emails): send all emails en masse

--- drchrono_birthdays/management/commands/send_emails.py
+++ drchrono_birthdays/management/commands/send_emails.py
@@ -4,12 +4,14 @@ from django.core.mail import send_mass_mail
 
 import datetime
 
+from drchrono_project.settings import EMAIL_HOST_USER
+
 class Command(BaseCommand):
     def handle(self, *args, **kwargs):
         def get_bday_emails(doctor):
             emails = []
-            patient_set = doctor.patient_set.filter(send_email=True)
-            for patient in patient_set.all():
+            patients = doctor.patient_set.filter(send_email=True)
+            for patient in patients:
                 if is_birthday(patient):
                     message = (generate_email(doctor, patient))
                     emails.append(message)
@@ -26,19 +28,27 @@ class Command(BaseCommand):
 
         def generate_email(doctor, patient):
             subject = doctor.email_subject.format(doctor.last_name)
-            subject = subject.replace('[first name]', patient.first_name)
-            subject = subject.replace('[last name]', patient.last_name)
+            subject = subject.replace(
+                '[first name]', patient.first_name
+            ).replace(
+                '[last name]', patient.last_name
+            )
 
             body = doctor.email_body.format(doctor.last_name)
-            body = body.replace('[first name]', patient.first_name)
-            body = body.replace('[last name]', patient.last_name)
+            body = body.replace(
+                '[first name]', patient.first_name
+            ).replace(
+                '[last name]', patient.last_name
+            )
 
-            message = (subject, body, 'edashmore@gmail.com',
+            message = (subject, body, EMAIL_HOST_USER,
                        [patient.email]
                       )
             return message
 
         doctors = Doctor.objects.all()
+        emails = []
         for doctor in doctors:
-            emails = get_bday_emails(doctor)
-            send_mass_mail(tuple(emails), fail_silently=False)
+            emails.extend(get_bday_emails(doctor))
+
+        send_mass_mail(tuple(emails), fail_silently=False)
